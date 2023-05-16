7ece14c652b837cd4b79c19300037b101b3f194d
Trasca Damian <damiantrasca@gmail.com>
2023-05-16 19:46:56 +0300

    Configure database connection and database scheme

---
+++ OnlineShopPcComponents/src/main/java/com/sda/onlineshop/componentspc/ComponentspcApplication.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshop.componentspc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentspcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComponentspcApplication.class, args);
+    }
+
+}
--- OnlineShopPcComponents/src/main/java/com/sda/onlineshop/componentspc/PcComponentsOnlineShop.java
+++
@@ -1,7 +0,0 @@
-package com.sda.onlineshop.componentspc;
-
-public class PcComponentsOnlineShop {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
