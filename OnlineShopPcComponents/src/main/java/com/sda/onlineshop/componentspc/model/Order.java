e00b854b5e756215eb55827ce438a5e5e05c3195
Trasca Damian <damiantrasca@gmail.com>
2023-05-16 20:55:41 +0300

    Create dummy index page

--- OnlineShopPcComponents/src/main/java/com/sda/onlineshop/componentspc/model/Order.java
+++ OnlineShopPcComponents/src/main/java/com/sda/onlineshop/componentspc/model/Order.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "order")
+@Table(name = "client_order")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
